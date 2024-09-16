4551cb303900869c0ebe2194aabf86a8f4cdf10f
Myrlie <myrlierenard@gmail.com>
2024-09-16 12:49:58 +0100

    first commit

---
+++ src/test_git/Test_Git.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test_git;
+
+/**
+ *
+ * @author Esther Myrlie Renard
+ */
+public class Test_Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       System. out. println("Hello world");
+    }
+    
+}
